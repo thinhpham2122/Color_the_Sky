e61bab718f78ba564d4028e305d989cdf0c92bb1
Thinh Pham <thinhpham2122@gmail.com>
2020-10-17 14:46:19 -0400

    test_model

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import numpy as np
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout, Conv2D
 
 number_of_image = 1
 path_to_feature = "/feature"
@@ -35,8 +37,32 @@ def get_features_labels():
     return features, labels
 
 
+def get_model(model_name=None):
+    def create_model():
+        model = Sequential()
+        model.add(Conv2D(16, (3, 3), activation='relu', inputshape=(512, 512, 3)))
+        model.add(Conv2D(1, (3, 3)))
+        model.compile(optimizer='adam', loss='mse')
+        return model
+
+    if model_name:
+        try:
+            model_l = load_model('keras_model/' + model_name)
+            model_l.compile(optimizer='adam', loss='mse')
+            print('Sucessfully loaded model')
+            exit()
+            return model_l
+        except:
+            print('Failed to load model. Creating new model')
+            return create_model()
+    else:
+        print('Creating new model')
+        return create_model()
+
+
 def main():
     features, labels = get_features_labels()
+    get_model('test_model')
 
 
 if __name__ == '__main__':
